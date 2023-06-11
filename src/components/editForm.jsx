2e1e67b24a6d5384a73fea9427fc3cc0fe465d9f
JinHe <18791461386@163.com>
2023-06-11 19:18:35 +0800

    Antd v4 and editing form component added

--- src/components/editForm.jsx
+++ src/components/editForm.jsx
@@ -1,13 +1,34 @@
-import { Button, Modal } from 'antd';
+import { Button, Modal, Input, Select, message } from 'antd';
 import React, { useState } from 'react';
+import './editForm.css';
+
 const EditForm = () => {
   const [loading, setLoading] = useState(false);
   const [open, setOpen] = useState(false);
+  const [data, updateData] = useState({
+    name: 'Jin',
+    age: 20,
+    country: 'China',
+  });
+  const handleValueChange = (propery, value) => {
+    const reg = /^-?\d*$/;
+    if (propery === 'age') {
+      if (reg.test(value) || value === '') {
+        data[propery] = value;
+        updateData({ ...data });
+      }
+    } else {
+      data[propery] = value;
+      updateData({ ...data });
+    }
+  }
   const showModal = () => {
     setOpen(true);
   };
   const handleOk = () => {
     setLoading(true);
+    message.success('Submitting data... View the latest data in the browser Console.');
+    console.log("New data is:", data);
     setTimeout(() => {
       setLoading(false);
       setOpen(false);
@@ -19,36 +40,66 @@ const EditForm = () => {
   return (
     <>
       <Button type="primary" onClick={showModal}>
-        Open Modal with customized footer
+        Open Edit Form
       </Button>
       <Modal
         open={open}
-        title="Title"
+        title="Edit"
         onOk={handleOk}
         onCancel={handleCancel}
         footer={[
-          <Button key="back" onClick={handleCancel}>
-            Return
-          </Button>,
           <Button key="submit" type="primary" loading={loading} onClick={handleOk}>
             Submit
           </Button>,
           <Button
-            key="link"
-            href="https://google.com"
+            key="cancel"
             type="primary"
-            loading={loading}
-            onClick={handleOk}
+            onClick={handleCancel}
           >
-            Search on Google
+            Cancel
           </Button>,
         ]}
       >
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+        <div className="form-item">
+          <div className="title">
+            Name:
+          </div>
+          <div className="input-wrap">
+            <Input placeholder="Name" value={data['name']} onChange={(e) => handleValueChange('name', e.target.value)} />
+          </div>
+        </div>
+        <div className="form-item">
+          <div className="title">
+            Age:
+          </div>
+          <div className="input-wrap">
+            <Input placeholder="Age" value={data['age']} onChange={(e) => handleValueChange('age', e.target.value)} />
+          </div>
+        </div>
+        <div className="form-item">
+          <div className="title">
+            Country:
+          </div>
+          <div className="input-wrap">
+            <Select
+              value={data['country']}
+              style={{
+                width: 472,
+              }}
+              onChange={(v) => handleValueChange('country', v)}
+              options={[
+                {
+                  value: 'China',
+                  label: 'China',
+                },
+                {
+                  value: 'Rassia',
+                  label: 'Rassia',
+                },
+              ]}
+            />
+          </div>
+        </div>
       </Modal>
     </>
   );
